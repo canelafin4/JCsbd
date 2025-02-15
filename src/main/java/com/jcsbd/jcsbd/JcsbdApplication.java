eb580691a4639d6a290fc311152aecc5b42521af
João Carlos Aguiar Silva (turma C) <jcaguiarsilva2005@gmail.com>
2025-02-15 13:55:14 -0300

    projeto iniciado

---
+++ src/main/java/com/jcsbd/jcsbd/JcsbdApplication.java
@@ -0,0 +1,13 @@
+package com.jcsbd.jcsbd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JcsbdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JcsbdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jcsbd/jcsbd/JcsbdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jcsbd.jcsbd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JcsbdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
